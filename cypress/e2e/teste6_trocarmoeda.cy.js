859240fcb77548fbc29ba163a53ed14054785245
matheusmartins5 <matheusmartins@ienh.com.br>
2024-11-18 22:34:22 -0300

    Moeda

--- cypress/e2e/teste6_trocarmoeda.cy.js
+++ cypress/e2e/teste6_trocarmoeda.cy.js
@@ -1,13 +1,22 @@
 describe('Trocar moeda do site', () => {
-  it('Deve trocar a moeda de exibição dos preços', () => {
+  it('Deve trocar a moeda de exibição dos preços para Euro, Libra e Dólar', () => {
     cy.visit('/');
-    cy.get('.btn-group').contains('Currency').click();
-    cy.contains('Euro').click();
-    cy.wait(2000);
-    cy.get('.price').first().should('contain', '€');
 
-    //Teste
+    cy.get('a.dropdown-toggle').contains('Currency').click();
+
+    cy.contains('Euro').click();
+    cy.wait(2000); 
+    cy.get('.price').first().should('contain', '€'); 
+
+    cy.get('a.dropdown-toggle').contains('Currency').click();
+    cy.contains('Pound Sterling').click(); 
+    cy.wait(2000); 
+    cy.get('.price').first().should('contain', '£'); 
+
+    cy.get('a.dropdown-toggle').contains('Currency').click();
+    cy.contains('US Dollar').click(); 
+    cy.wait(2000); 
+    cy.get('.price').first().should('contain', '$'); 
   });
 });
-
 
