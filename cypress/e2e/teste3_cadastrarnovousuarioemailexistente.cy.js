caf896aa8867e6bbb1d54fe0a761450457537ace
matheusmartins5 <matheusmartins@ienh.com.br>
2024-11-20 21:30:40 -0300

    a

--- cypress/e2e/teste3_cadastrarnovousuarioemailexistente.cy.js
+++ cypress/e2e/teste3_cadastrarnovousuarioemailexistente.cy.js
@@ -1,2 +1,20 @@
+describe('Cadastrar novo usuário com o e-mail de um usuário existente', () => {
+    it('Não deve permitir cadastrar e exibir mensagem de erro', () => {
+      cy.visit('/index.php?route=account/register');
+  
+      cy.wait(3000);
+  
+      cy.get('#input-firstname').type('Matheus');
+      cy.get('#input-lastname').type('Martins');
+      cy.get('#input-email').type('matheusmartins@ienh.com.br');
+      cy.get('#input-password').type('senha1234');
+      cy.get('input[name="agree"]').check();
 
+      cy.contains('Continue').click();
+      cy.wait(2000);
+  
+      cy.get('h1').should('contain', 'Your Account Has Been Created!');
+      cy.contains('Congratulations! Your new account has been successfully created!').should('be.visible');
+    });
+  });
 
