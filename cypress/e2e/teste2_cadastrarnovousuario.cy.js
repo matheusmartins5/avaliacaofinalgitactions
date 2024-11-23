1ba739b4332564c2d3622b09e7ea01f4ab6ab321
matheusmartins5 <matheusmartins@ienh.com.br>
2024-11-23 15:21:18 -0300

    aa

--- cypress/e2e/teste2_cadastrarnovousuario.cy.js
+++ cypress/e2e/teste2_cadastrarnovousuario.cy.js
@@ -1,5 +1,5 @@
 describe('Cadastrar novo usuário', () => {
-  it('Deve realizar cadastro de um novo usuário com sucesso', () => {
+  it('Deve realizar o cadastro com sucesso', () => {
     cy.visit('/');
 
     cy.get('a').contains('My Account').click();
@@ -7,18 +7,22 @@ describe('Cadastrar novo usuário', () => {
 
     cy.wait(3000);
 
-    const uniqueEmail = `usuario${Date.now()}@ienh.com`;
+    const randomUsername = `user_${Cypress._.random(0, 1e6)}`;
+    const randomPassword = `pass_${Cypress._.random(0, 1e6)}`;
+    const randomEmail = `${randomUsername}@ienh.com`;
 
-    cy.get('#input-firstname').type('Matheus'), { delay: 100 };
-    cy.get('#input-lastname').type('Martins'), { delay: 100 };
-    cy.get('#input-email').type(uniqueEmail), { delay: 100 };
-    cy.get('#input-password').type('senha1234');
+    cy.get('#input-firstname').type('Matheus', { delay: 100 });
+    cy.get('#input-lastname').type('Martins', { delay: 100 });
+    cy.get('#input-email').type(randomEmail, { delay: 100 });
+    cy.get('#input-password').type(randomPassword, { delay: 100 });
     cy.get('input[name="agree"]').check();
 
     cy.contains('Continue').click();
+
     cy.wait(2000);
 
     cy.get('h1').should('contain', 'Your Account Has Been Created!');
     cy.contains('Congratulations! Your new account has been successfully created!').should('be.visible');
   });
 });
+
